f61f5185b4c3415aeb5d7d08feb38610947d4748
Vidyasekaran <vidguru1008@gmail.com>
2020-11-04 15:05:11 +0530

    1st commit

--- 03-todo-web-application-mysql/src/main/java/com/in28minutes/springboot/web/controller/LogoutController.java
+++ 03-todo-web-application-mysql/src/main/java/com/in28minutes/springboot/web/controller/LogoutController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LogoutController {
+	
 
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(HttpServletRequest request,
